ade3ac1cda2ea53c807d1cc5a613118e49b5c22c
GvozdenovicDjuradj <gdjura@ymail.com>
2023-03-08 18:33:48 +0100

    added date formats to swagger documentation

--- src/main/java/com/rbt/searcher/controller/VacationDaysController.java
+++ src/main/java/com/rbt/searcher/controller/VacationDaysController.java
@@ -4,6 +4,7 @@ import com.rbt.searcher.model.dto.AddedUsedDaysResponse;
 import com.rbt.searcher.model.dto.TotalDaysResponse;
 import com.rbt.searcher.model.dto.UsedDaysResponse;
 import com.rbt.searcher.service.VacationDaysService;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.AllArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -24,19 +25,23 @@ public class VacationDaysController {
 
     @GetMapping(value = "total-days/{year}", produces = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("hasRole('USER')")
-    public ResponseEntity<TotalDaysResponse> getTotalDays(@PathVariable Integer year){
+    public ResponseEntity<TotalDaysResponse> getTotalDays(@PathVariable Integer year) {
         return ResponseEntity.ok(vacationDaysService.getTotalDays(year));
     }
 
     @GetMapping(value = "used-days", produces = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("hasRole('USER')")
-    public ResponseEntity<UsedDaysResponse> getUsedDays(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate from, @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate to){
+    public ResponseEntity<UsedDaysResponse> getUsedDays(
+            @RequestParam @Schema(description = "From date in yyyy-MM-dd format", type = "string", format = "date", example = "2022-12-01") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate from,
+            @RequestParam @Schema(description = "To date in yyyy-MM-dd format", type = "string", format = "date", example = "2022-12-01") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate to) {
         return ResponseEntity.ok(vacationDaysService.getUsedDays(from, to));
     }
 
     @PostMapping(value = "used-days", produces = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("hasRole('USER')")
-    public ResponseEntity<AddedUsedDaysResponse> addUsedDays(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate from, @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate to){
+    public ResponseEntity<AddedUsedDaysResponse> addUsedDays(
+            @RequestParam @Schema(description = "From date in yyyy-MM-dd format", type = "string", format = "date", example = "2022-12-01") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate from,
+            @RequestParam @Schema(description = "To date in yyyy-MM-dd format", type = "string", format = "date", example = "2022-12-01") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate to) {
         return ResponseEntity.ok(vacationDaysService.addUsedDays(from, to));
     }
 }
